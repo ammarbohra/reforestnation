b482539539de0a3fdffece7715b7493d19a9ac07
Himanshu Dhaka <himanshudhaka1701@gmail.com>
2021-10-08 00:05:24 +0530

    Hero Section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-console.log('haha')
+import 'bootstrap'
+import { gsapInit } from './gsap'
+
+window.addEventListener('load', () => {
+    gsapInit()    
+})
+
+const myNav = document.querySelector('.navbar');
+const navItem = document.getElementsByClassName('nav-item');
+
+console.log(navItem);
+
+window.addEventListener('scroll',() => {
+    // console.log(window.scrollY)
+    if (window.scrollY >= 100 ) {
+        myNav.classList.add("bg-white");
+        myNav.classList.add("navbar-light");
+        myNav.classList.remove("navbar-dark");
+    } 
+    else {
+        myNav.classList.add("navbar-dark");
+        myNav.classList.remove("bg-white");
+        myNav.classList.remove("navbar-light");
+    }
+})
