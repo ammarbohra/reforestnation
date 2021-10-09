4f3817973995fd570dfbb73c9fccbfdbf6763704
Himanshu Dhaka <himanshudhaka1701@gmail.com>
2021-10-09 15:48:32 +0530

    Crowd Page

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,14 +1,14 @@
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 
-// export function gsapInit() {
-//     const heroContent = document.querySelector('.hero-content').children
-//     gsap.timeline()
-//         .from('.nav-item',{ y: -200 , stagger: 0.1 , duration:1})
-//         .from('.phone',{x: 700 , y: 700})
-//         .from(heroContent,{ x:-1000 , stagger:0.4 })
+export function gsapInit() {
+    const heroContent = document.querySelector('.hero-content').children
+    gsap.timeline()
+        .from('.nav-item',{ y: -200 , stagger: 0.1 , duration:1})
+        .from('.phone',{x: 700 , y: 700})
+        .from(heroContent,{ x:-1000 , stagger:0.4 })
 
-// }
+}
